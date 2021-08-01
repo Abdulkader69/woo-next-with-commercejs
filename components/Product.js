152c5fc8e581bc0bf49970fae3d3926cb39fd53d
Abdul Kader <>
2021-08-01 16:05:40 +0600

    product

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,9 @@
 export default function Product({ name, price, media }) {
   return (
     <div className="product-grid-inner">
-      <div className="thumbnail flex h-60 overflow-hidden"><img className="object-cover object-center w-full transition-all transform hover:scale-110" src={media.source} alt={name} /></div>
+      <div className="thumbnail flex h-60 overflow-hidden">
+        <img className="object-cover object-center w-full transition-all transform hover:scale-110" src={media.source} alt={name} />
+      </div>
       <h3 className="text-xl mt-5 mb-2">{name}</h3>
       <p className="text-sm">{price.formatted_with_symbol}</p>
     </div>
--- pages/products/[permalink].js
+++ pages/products/[permalink].js
@@ -44,17 +44,17 @@ export default function ProductPage({ product }) {
       <div className="container">
         <div className="single-product-wrapper flex flex-wrap -mx-4">
           <div className="w-6/12 px-4">
-            <div className="product-thumbnail flex h-[500px] overflow-hidden">
+            <div className="product-thumbnail flex overflow-hidden">
               <img className="object-cover w-full" src={product.media.source} alt={product.name} />
             </div>
           </div>
-          <div className="w-6/12 px-4">
-            <h1 className="text-[35px] font-medium">{product.name}</h1>
-            <p className="text-lg">{product.price.formatted_with_symbol}</p>
-            <div className="description">
+          <div className="w-6/12 px-4 mt-10">
+            <h1 className="font-bold text-5xl mb-4">{product.name}</h1>
+            <p className="text-2xl font-medium mb-3">{product.price.formatted_with_symbol}</p>
+            <div className="description mb-5">
               {parse(product.description)}
             </div>
-            <button onClick={addToCart}>Add to Cart</button>
+            <button className="bg-red-400 py-2 px-6 rounded-md text-white hover:bg-red-500 transition-all" onClick={addToCart}>Add to Cart</button>
           </div>
         </div>
       </div>
